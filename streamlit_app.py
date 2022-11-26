9a58ec57185a03c2f04819822b61e31a2fd06eea
Syahid <92115178+syahidhusein@users.noreply.github.com>
2022-11-26 22:09:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,9 @@ else:
 st.header("Selectbox from a NumPy array")
 array = np.array([[1,2,3],[4,5,6],[7,8,9]])
 col1, mid, col2 = st.columns([1,0.1,3])
+print(col1)
+print(mid)
+print(col2)
 
 """File uploader"""
 st.file_uploader("Please upload a file")
