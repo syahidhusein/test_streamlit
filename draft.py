6b230d4e57be3612d7420ff5e31bb6ab0bb44718
Syahid <92115178+syahidhusein@users.noreply.github.com>
2022-12-07 15:25:10 +0100

    Update draft.py

--- draft.py
+++ draft.py
@@ -60,7 +60,6 @@ if st.session_state["photo"]=="done":
       else:
         st.image(item)
 st.write(camera_pic)
-camera_pic.save(".")
 st.write(type(camera_pic))
 if camera_pic is not None:
     # To read image file buffer as a PIL Image:
