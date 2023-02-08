7c0c131b6e38191a33a9c2f403f81487d08b0333
Syahid Husein <92115178+syahidhusein@users.noreply.github.com>
2023-02-08 16:21:28 +0100

    Update draft.py

--- draft.py
+++ draft.py
@@ -46,7 +46,7 @@ with col3:
   with title:
     st.title("Outfit Recommender")
 
-with col2:
+with col1:
     st.title("Introduction")
    
 st.write("This project aims to develop an outfit recommendation system using deep learning techniques. The system will take an image from the user as input and suggest a suitable outfit for them. The model will learn to recognize patterns and features in the images, allowing it to make accurate recommendations.")
